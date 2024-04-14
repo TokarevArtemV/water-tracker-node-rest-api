473b4fbf30f48bc02b064c80f7a96f40c4f14f74
Artem <135842119+TokarevArtemV@users.noreply.github.com>
2024-04-14 17:56:01 +0300

    Update waterPortionServices.js
    change WaterModal on Water

--- services/waterPortionServices.js
+++ services/waterPortionServices.js
@@ -1,6 +1,6 @@
-import WaterModal from "../models/Water.js";
+import Water from "../models/Water.js";
 
-const addWaterPortion = (data) => WaterModal.create(data);
+const addWaterPortion = (data) => Water.create(data);
 
 export default {
     addWaterPortion,
