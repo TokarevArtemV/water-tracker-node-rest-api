d34d7608a22b4b29762914fb5835b74b1ca88fac
Artem Tokarev <tokarev.artem.v@gmail.com>
2024-04-16 01:31:21 +0300

    fix cors 3

--- app.js
+++ app.js
@@ -12,14 +12,27 @@ const { PORT = 3000, DB_HOST } = process.env;
 const app = express();
 
 app.use(morgan("tiny"));
-app.use(cors());
-app.use((_, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://water-tracker-pi.vercel.app/",
+    "https://water-tracker-node-rest-api.onrender.com",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
 });
+
 app.use(express.json());
 app.use(express.static("public"));
 
