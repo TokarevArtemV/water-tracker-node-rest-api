3ce1c37d6001a3dc59e95763961d4d7d2b0160a9
Artem Tokarev <tokarev.artem.v@gmail.com>
2024-04-16 10:54:59 +0300

    fix cors 9

--- middlewares/customCors.js
+++ middlewares/customCors.js
@@ -1,14 +1,14 @@
 const customCors = (req, res, next) => {
-  //   res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "https://water-tracker-pi.vercel.app",
-    "https://water-tracker-node-rest-api.onrender.com",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+  res.header("Access-Control-Allow-Origin", "*");
+  //   const allowedOrigins = [
+  //     "http://localhost:5173",
+  //     "https://water-tracker-pi.vercel.app",
+  //     "https://water-tracker-node-rest-api.onrender.com",
+  //   ];
+  //   const origin = req.headers.origin;
+  //   if (allowedOrigins.includes(origin)) {
+  //     res.setHeader("Access-Control-Allow-Origin", origin);
+  //   }
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
