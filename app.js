c8a53c6893d77c9220d8ab9a11a1fda8f8d30a54
Artem Tokarev <tokarev.artem.v@gmail.com>
2024-04-16 10:23:43 +0300

    fix cors 6

--- app.js
+++ app.js
@@ -1,37 +1,18 @@
 import express from "express";
 import morgan from "morgan";
-import cors from "cors";
 import mongoose from "mongoose";
-
-import userRouter from "./routes/userRouter.js";
-import "dotenv/config.js";
-import waterPortionsRouter from "./routes/waterPortionsRouter.js";
+import customCors from "./middlewares/customCors.js";
 import authenticate from "./middlewares/authenticate.js";
+import userRouter from "./routes/userRouter.js";
+import waterPortionsRouter from "./routes/waterPortionsRouter.js";
+import "dotenv/config.js";
 
 const { PORT = 3000, DB_HOST } = process.env;
 const app = express();
 
 app.use(morgan("tiny"));
 
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "https://water-tracker-pi.vercel.app",
-    "https://water-tracker-node-rest-api.onrender.com",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  next();
-});
+app.use(customCors);
 
 app.use(express.json());
 app.use(express.static("public"));
