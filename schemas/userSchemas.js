179605f8b163ac042a628b7558c49529d4f2ad67
Artem Tokarev <tokarev.artem.v@gmail.com>
2024-04-18 14:27:06 +0300

    added user current controller

--- schemas/userSchemas.js
+++ schemas/userSchemas.js
@@ -1,37 +1,40 @@
 import Joi from "joi";
-import { EMAIL_REGEX , PASSWORD_REGEX} from "../constants/regexPatterns.js";
-import {EMAIL_ERROR_MESSAGES, PASSWORD_ERROR_MESSAGES} from "../constants/userConstants.js";
+import { EMAIL_REGEX, PASSWORD_REGEX } from "../constants/regexPatterns.js";
+import {
+  EMAIL_ERROR_MESSAGES,
+  PASSWORD_ERROR_MESSAGES,
+} from "../constants/userConstants.js";
 
 const emailField = {
   email: Joi.string()
-  .pattern(EMAIL_REGEX)
-  .required()
-  .messages(EMAIL_ERROR_MESSAGES),
-}
+    .pattern(EMAIL_REGEX)
+    .required()
+    .messages(EMAIL_ERROR_MESSAGES),
+};
 
 const passwordField = {
   password: Joi.string()
-  .min(8)
-  .max(64)
-  .pattern(PASSWORD_REGEX)
-  .required()
-  .messages(PASSWORD_ERROR_MESSAGES),
+    .min(8)
+    .max(64)
+    .pattern(PASSWORD_REGEX)
+    .required()
+    .messages(PASSWORD_ERROR_MESSAGES),
 };
 
 const authSchema = Joi.object({
   ...emailField,
-  ...passwordField
+  ...passwordField,
 });
 
 const userDataUpdateSchema = Joi.object({
   ...emailField,
   ...passwordField,
   oldPassword: Joi.string()
-  .min(8)
-  .max(64)
-  .pattern(PASSWORD_REGEX)
-  .required()
-  .messages(PASSWORD_ERROR_MESSAGES),
+    .min(8)
+    .max(64)
+    .pattern(PASSWORD_REGEX)
+    .required()
+    .messages(PASSWORD_ERROR_MESSAGES),
   gender: Joi.string().valid("male", "female"),
   username: Joi.string().max(32),
 });
@@ -44,9 +47,14 @@ const userVerifyEmailSchema = Joi.object({
   ...emailField,
 });
 
+const userTokenSchema = Joi.object({
+  token: Joi.string().required(),
+});
+
 export default {
   authSchema,
   userDataUpdateSchema,
   userWaterRateSchema,
   userVerifyEmailSchema,
+  userTokenSchema,
 };
